530fc917dd3584aed65d7af0ec63bab7c899aa4d
rendijulianto <rendijulianto37@gmail.com>
2023-11-20 13:33:38 +0700

    Update : Main

--- src/Main.php
+++ src/Main.php
@@ -3,8 +3,51 @@ namespace Rendijulianto\Piwapi;
 
 class Main
 {
-    public function __construct()
+    private $url;
+    private $apikey;
+    private $sender;
+
+    // Constructor
+    public function __construct($url, $apikey, $sender) {
+        $this->url = $url;
+        $this->apikey = $apikey;
+        $this->sender = $sender;
+    }
+
+    // Send Message
+    public function sendMessage($receiver, $message)
     {
-        echo "Hello World!";
+        $postData = [
+
+            "secret" => $this->apikey,
+            "account" => $this->sender,
+            "recipient" => $receiver,
+            "type" => "text",
+            "priority" => 1,
+            "message" => $message,
+        ];
+
+        $cURL = curl_init($this->url);
+        curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($cURL, CURLOPT_POSTFIELDS, $postData);
+        $response = curl_exec($cURL);
+        curl_close($cURL);
+
+        $result = json_decode($response, true);
+        return $result;
+    }
+    
+    public function checkNumber($phone)
+    {
+        if (substr($phone, 0, 2) != "62") {
+            return false;
+        }
+        if (strlen($phone) < 10 || strlen($phone) > 15) {
+            return false;
+        }
+        if (!is_numeric($phone)) {
+            return false;
+        }
+        return true;
     }
 }
